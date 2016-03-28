7064fc10a183a8088c28a56d2d97f27eae708914
Wei Hsien Yeh <whyeh@mosi.com.tw>
2016-03-28 16:46:02 +0800

    ui tweak

--- ui.R
+++ ui.R
@@ -4,6 +4,11 @@ library(ggplot2)
 shinyUI(
     fluidPage(
         titlePanel("Manufacturing Margin and Cost Calculator"),
+        br(),
+        h4("In this app, we will use information you have provided to calculate the revenue you should obtain for the
+           product you have made, losses that have occurred at the shop floor, and the net profit you should end up 
+           with."),
+        br(),
         fluidRow(
             h4("Input values for variables that do not change from job to job"),
             column(3, 
@@ -19,20 +24,24 @@ shinyUI(
         br(),
         
         fluidRow(
-            h4("Input values for variables pertaining to your first inquiry"),
-            column(3,
+            column(4,
                    # textInput("inquiry1", label = "Inquiry Code", value = "Shift #, Product ID, etc..."),
-                   numericInput("price1", label = "Product Price to Customer", value = 3.48),
+                   h4("Input values associated with the product produced"),
+                   numericInput("price1", label = "Unit Price to Customer", value = 3.48),
                    numericInput("material1", label = "Material Cost", value = 1.9),
                    numericInput("direct1", label = "Direct Burden per Part", value = 0.25),
-                   numericInput("num.operators1", label = "# of Operators Assigned", value = 4),
+                   br(),
+                   h4("Input production recipe"),
+                   numericInput("num.operators1", label = "# of Operators Needed", value = 4),
                    numericInput("ct1", label = "Theoretical Output CT (in sec)", value = 15),
-                   numericInput("duration1", label = "Total Duration (in minutes)", value = 480),
-                   numericInput("produced1", label = "# of Product Produced", value = 1280),
+                   br(),
+                   h4("Input manufacturing data"),
+                   numericInput("duration1", label = "Total Production Duration (in minutes)", value = 480),
+                   numericInput("produced1", label = "# of Units Produced", value = 1280),
                    numericInput("planned1", label = "Planned Downtime (in minutes)", value = 50),
                    numericInput("downtime1", label = "Unplanned Downtime (in minutes)", value = 73),
-                   numericInput("NG1", label = "# of NG outputs", value = 12),
-                   actionButton("button1", "Show !")
+                   numericInput("NG1", label = "# of NG outputs", value = 12)
+                   # actionButton("button1", "Show !")
             ), 
             column(8,
                    h4("Revenue to Net Profit Waterfall Chart"),
